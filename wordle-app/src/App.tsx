47a180942eed9f4c83936911dec944d956acef20
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2023-10-12 17:32:33 +0200

    fix bug

--- wordle-app/src/App.tsx
+++ wordle-app/src/App.tsx
@@ -2,10 +2,10 @@ import { useContext } from 'react';
 import { MainPage } from './components/MainPage/MainPage';
 import './App.scss';
 import { ThemeContext } from './components/useContext/ThemeContext';
-import { ContextType } from './components/DarkModeToggle/DarkModeToggle';
+import { DarkModeContextType } from './components/DarkModeToggle/DarkModeToggle';
 
 export const App = () => {
-  const { darkMode } = useContext<ContextType>(ThemeContext);
+  const { darkMode } = useContext<DarkModeContextType>(ThemeContext);
 
   return (
     <div className="App" id={darkMode ? 'darkMode' : ''}>
